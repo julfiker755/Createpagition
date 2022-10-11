18075501bfad99f8f1b645cda98d751a8089bba0
Julfiker Rahaman Jim <97047069+julfiker755@users.noreply.github.com>
2022-10-11 11:45:05 +0600

    Add files via upload
    creare

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import Shop from "./Component/Shop/Shop";
+
+
+function App() {
+  return (
+    <div className="App">
+     <Shop></Shop>
+    </div>
+  );
+}
+
+export default App;
